fee386a5071ba35b2876f865808f274794c44840
takaji <miukanaseiji@gmail.com>
2016-04-25 16:57:36 +0900

    Create Exam1_1.java

---
+++ Exam1_1.java
@@ -0,0 +1,58 @@
+
+import java.util.Optional;
+import java.util.function.BiPredicate;
+
+/**
+ * Exam 1-1
+ */
+public class Exam1_1 {
+	public static void main(String[] args) {
+		final String input = "134297381";
+		final Optional<String> result = maxSubstrWithCond(input, (left, right) -> {
+			return Integer.valueOf(left) < Integer.valueOf(right);
+		});
+
+		System.out.println(result.orElse("none"));
+	}
+
+	/***
+	 * 隣り合う文字が連続して条件を満たす最長の部分文字列を返す。<br/>
+	 * 同一の長さの部分文字列があった場合は先頭の部分文字列を返す。
+	 * @param inStr 対象文字列
+	 * @param cond 隣り合う2つの文字を引数にブール値を返すBiPredicateインターフェースオブジェクト
+	 * @return　部分文字列
+	 */
+	public static Optional<String> maxSubstrWithCond(String inStr, BiPredicate<String, String> cond) {
+		int beginIdx = -1;
+		int endIdx = -1;
+		int maxLen = 0;
+		int currentBeginIdx = -1;
+		int currentEndIdx = -1;
+		int currentMaxLen = 0;
+		for(int i = 0; i < inStr.length() - 1; i++) {
+			if(cond.test(String.valueOf(inStr.charAt(i)), String.valueOf(inStr.charAt(i + 1)))) {
+				if(currentBeginIdx < 0) {
+					currentBeginIdx = i;
+				}
+				currentEndIdx = i + 2;
+				currentMaxLen += 1;
+			} else {
+				if(maxLen < currentMaxLen) {
+					maxLen = currentMaxLen;
+					beginIdx = currentBeginIdx;
+					endIdx = currentEndIdx;
+				}
+				currentBeginIdx = -1;
+				currentMaxLen = 0;
+			}
+		}
+		
+		if(maxLen < currentMaxLen) {
+			maxLen = currentMaxLen;
+			beginIdx = currentBeginIdx;
+			endIdx = currentEndIdx;
+		}
+		
+		return maxLen == 0 ? Optional.ofNullable(null) : Optional.of(inStr.substring(beginIdx, endIdx));
+	}
+}
